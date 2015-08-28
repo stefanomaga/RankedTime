1c6a9ba1663457422f88768ddd6077396bce8904
stefanomaga <stefano.maga@outlook.com>
2015-08-28 21:05:31 +0200

    Model: Champion / Summoner

---
+++ RankedTime/src/main/java/model/Champion.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Champion {
+
+	private String name;
+	private String role;
+	
+	public Champion(String name, String role) {
+		super();
+		this.name = name;
+		this.role = role;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
---
+++ RankedTime/src/main/java/model/Summoner.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.List;
+
+public class Summoner {
+	
+	private String username;
+	private String password;
+	private List<Champion> champions;
+
+	public Summoner(String username, String password, List<Champion> champions) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.champions = champions;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Champion> getChampions() {
+		return champions;
+	}
+
+	public void setChampions(List<Champion> champions) {
+		this.champions = champions;
+	}
+
+	
+}
